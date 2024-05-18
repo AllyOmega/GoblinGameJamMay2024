e6588266abe0916d8a19de1951829d716e5b3f3b
Andrew <mooo3890@gmail.com>
2024-05-18 16:02:51 -0400

    implement score and target range

--- source/main.lua
+++ source/main.lua
@@ -9,6 +9,11 @@ local gfx<const> = pd.graphics
 local fillHeight = -20
 local fillIncr = -1
 local progressPercent = 10
+local score = 0
+local targetRangeMax = 55
+local targetRangeMin = 45
+local tickCount = 0
+
 
 function setupGame()
 
@@ -40,13 +45,18 @@ function setupGame()
 
     progressSprite = gfx.sprite.new( progressImage[3] )
     progressSprite:moveTo( 375, 120  )
-    progressSprite:setRotation(180)
 	updateProgress()
     progressSprite:add()
 
     surroundSprite = gfx.sprite.new( progressImage[2] )
     surroundSprite:moveTo( 375, 120  )
     surroundSprite:add()
+
+    local arrowImg = gfx.image.new('images/arrow')
+    targetArrow = gfx.sprite.new(arrowImg)
+    assert( targetArrow )
+    targetArrow:moveTo(354,120)
+    targetArrow:add()
 
 end
 
@@ -56,12 +66,35 @@ end
 
 function fillBar()
 
-    progressPercent += (pd.getCrankChange()//10)
+    progressPercent += (pd.getCrankChange()//8)
 	if progressPercent > 120 or progressPercent <= 0 then progressPercent = 0 end
 	updateProgress()
     progressPercent -= (math.random(5,10)//2)
 
 end
+ function gameLoop()
+
+    if progressPercent > (targetRangeMin+2) and progressPercent < (targetRangeMax-2)  then
+        tickCount += 1
+        if tickCount >= 4 then
+            score += 2
+        end
+    elseif progressPercent > targetRangeMin and progressPercent < targetRangeMax then
+        tickCount += 1
+        if tickCount >= 3 then
+            score += 1
+        end
+    else
+        tickCount = 0
+        if score >= 5 then
+            score -= 1
+        else
+            score = 0
+        end
+    end
+
+ end
+
 
 
 setupGame()
@@ -69,9 +102,10 @@ setupGame()
 function pd.update()
 
     fillBar()
+    gameLoop()
     gfx.sprite.update()
     playdate.timer.updateTimers()
-
+    gfx.drawText(tostring(score), 0, 220)
     pd.drawFPS(0, 0)
 
     if(pd.isCrankDocked()) then
